87aeb1d2c2c1910e4f9b53f3ec178649078a0113
Ryan Laurie <iethree@gmail.com>
2022-07-06 13:31:52 -0600

    update bulk filter layout

--- frontend/src/metabase/query_builder/components/QueryModals.jsx
+++ frontend/src/metabase/query_builder/components/QueryModals.jsx
@@ -150,7 +150,7 @@ export default class QueryModals extends React.Component {
         />
       </Modal>
     ) : modal === MODAL_TYPES.FILTERS ? (
-      <Modal onClose={onCloseModal}>
+      <Modal medium onClose={onCloseModal}>
         <BulkFilterModal question={question} onClose={onCloseModal} />
       </Modal>
     ) : modal === MODAL_TYPES.HISTORY ? (
